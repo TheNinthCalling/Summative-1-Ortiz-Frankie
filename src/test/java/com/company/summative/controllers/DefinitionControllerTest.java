4f115a8abbd8946c484ee4bfae8b67e2473ed3bd
Frankie Ortiz <frankie.ortiz2001@gmail.com>
2023-07-06 18:37:16 -0500

    Definition API done

--- src/test/java/com/company/summative/controllers/DefinitionControllerTest.java
+++ src/test/java/com/company/summative/controllers/DefinitionControllerTest.java
@@ -1,4 +1,32 @@
 package com.company.summative.controllers;
 
+import com.company.summative.models.Answer;
+import com.company.summative.models.Definition;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DefinitionController.class)
 public class DefinitionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void ShouldReturnWord() throws Exception {
+
+        mockMvc.perform(get("/word"))
+                .andDo(print())             //Print results
+                .andExpect(status().isOk()); //Assert (code is 200)
+
+    }
 }
